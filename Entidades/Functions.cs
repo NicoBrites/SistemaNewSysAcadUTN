1433bff633fb55220aa10d58e04802cee5559ef2
NicoBrites <nicolas.britesv@gmail.com>
2023-10-09 02:35:13 -0300

    Correccion proligidad

--- Entidades/Functions.cs
+++ Entidades/Functions.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-
+﻿
 namespace Entidades
 {
     public class Functions
---
+++ Proyecto_Grupal/Logic/Mail.cs
@@ -0,0 +1,43 @@
+﻿using MimeKit;
+
+namespace Logic
+{
+    public static class Email
+    {
+        /// <summary>
+        /// Envia un email de confirmacion al usuario registrado
+        /// </summary>
+        /// <param name="email"></param>
+        public static string SendMessageSmtp(string email, string contraseña, string nombre, string apellido)
+        {
+            MimeMessage mail = new MimeMessage();
+            mail.From.Add(new MailboxAddress("Sistema Sysacad", "foo@sandbox54c0a3c56b0042a2801b8f6c5cebe46a.mailgun.org"));
+            mail.To.Add(new MailboxAddress($"{apellido},{nombre}", email));
+            mail.Subject = "Registro de alumno";
+            mail.Body = new TextPart("plain")
+            {
+                Text = @$"Registro exitoso, bienvenido al nuevo SistemaSysacad. Tu contraseña es: {contraseña} y tu usario es tu DNI",
+            };
+            try
+            {
+                using (var client = new MailKit.Net.Smtp.SmtpClient())
+                {
+                    client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                    client.Connect("smtp.mailgun.org", 587, false);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate("postmaster@sandbox54c0a3c56b0042a2801b8f6c5cebe46a.mailgun.org", "6fe8556259578bfe346b768e1f8ff099-77316142-466e2907");
+
+                    client.Send(mail);
+                    client.Disconnect(true);
+                }
+            }
+            catch (Exception)
+            {
+
+                return "El mail registrado no se encuentra valido en mailgun";
+            }
+            return "Email entregado";
+        }
+    }
+}
